a4b90b57e1d3a9b63ff3128f8495038bca1dbc8b
Xavier <xdupondpoubelle@example.com>
2025-10-30 12:05:20 +0100

    v2.0.0

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
+const getWeatherOfTheDay = () => {
+  return "sunny";
+};
+
 function Home() {
-    return <h1>Hello from Home</h1>
+  const [weather, setWeather] = useState(null as string | null);
+
+  useEffect(() => {
+    const weatherOfTheDay = getWeatherOfTheDay();
+
+    setWeather(weatherOfTheDay);
+  }, []);
+
+  return (
+    <>
+      <h1>Hello from Home</h1>
+
+      {weather != null && <p>Today is a {weather} day</p>}
+    </>
+  );
 }
 
-export default Home
+export default Home;
